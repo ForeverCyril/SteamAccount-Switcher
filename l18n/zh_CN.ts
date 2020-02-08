0de7a1f9b5cebb3907530c9d873bb02c7a1aa4f4
ForeverCyril <cyrilchen1999@gmail.com>
2020-02-08 15:52:29 +0800

    添加中文翻译

---
+++ l18n/zh_CN.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="29"/>
+        <source>Steam Account Swithcer</source>
+        <translation>Steam账号切换器</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="47"/>
+        <source>User Name: </source>
+        <translation>用户名：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="116"/>
+        <source>Personal Name: </source>
+        <translatorcomment>最后登录</translatorcomment>
+        <translation>昵称：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="129"/>
+        <source>Last Login: </source>
+        <translation>最后登录：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="153"/>
+        <source>Exit After Login</source>
+        <translation>登录后关闭</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="176"/>
+        <source>Login</source>
+        <translation>登录</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="79"/>
+        <source>Steam is running</source>
+        <translation>Steam已经运行</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="80"/>
+        <source>Kill Steam process?</source>
+        <translation>结束Steam进程？</translation>
+    </message>
+</context>
+</TS>
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <QApplication>
+#include <QTranslator>
 #include "mainwindow.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+
+    if(QLocale().language() == QLocale::Chinese){
+        QTranslator *t = new QTranslator;
+        if(t->load("l18n/zh_CN.qm")) {
+            app.installTranslator(t);
+        }
+    }
+
     MainWindow w;
     w.show();
     return app.exec();
